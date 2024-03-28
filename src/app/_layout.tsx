729fb8e7920a0187672e765401f51923f7311eee
Matthew Monday <mondaym12@yahoo.com>
2024-03-28 08:42:19 +0100

    import global css in rootlayout and check for authentication

---
+++ src/app/_layout.tsx
@@ -0,0 +1,36 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { Slot, useRouter, useSegments } from 'expo-router'
+import "../../global.css"
+import { AuthContextProvider, useAuth } from '../context/authContext'
+// import { MenuProvider } from 'react-native-popup-menu'
+
+const MainLayout = () => {
+    const { isAuthenticated } = useAuth();
+    const segments = useSegments();
+    const router = useRouter();
+
+
+
+    useEffect(() => {
+        if (typeof isAuthenticated == "undefined") return;
+        const inApp = segments[0] == '(app)';
+        if (isAuthenticated && !inApp) {
+            router.replace('/home')
+        } else if (isAuthenticated == false) {
+            router.replace('/logIn')
+        }
+
+    }, [isAuthenticated])
+
+    return <Slot />
+}
+export default function RootLayout() {
+    return (
+        // <MenuProvider>
+        <AuthContextProvider>
+            <MainLayout />
+        </AuthContextProvider>
+        // </MenuProvider>
+    )
+}
